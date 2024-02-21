ebe750aad91cdbf1dc363cbd9fb9eacc4a73775e
Adam Bryan (Other) <adam.bryan@ubisoft.com>
2024-02-21 09:18:11 +0000

    setup background and a rectangle to represent the space ship at the bottom middle of the window. Sprite sheets added to assets but not yet added to the code

--- main.cpp
+++ main.cpp
@@ -6,12 +6,20 @@ int main()
     int windowHeight = 800;
     char Title[]{"SpaceHopper"}; 
 
+    //Space Ship Texture
+    Character shipData;
+    shipData.position.x = windowWidth / 2;
+    shipData.position.y = windowHeight - shipData.texture.height;
+
+
     InitWindow(windowWidth, windowHeight, Title);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
+
+        DrawRectangle(windowWidth/2, windowHeight-100, 50, 150, WHITE);
 
         EndDrawing();
     }
--- main.h
+++ main.h
@@ -1,1 +1,8 @@
 #include <raylib.h>
+
+struct Character
+{
+    Texture2D texture;
+    Rectangle rec;
+    Vector2 position;
+};
