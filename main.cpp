3c650215281a504e0ced8792be77ded409710c65
Adam Bryan (Other) <adam.bryan@ubisoft.com>
2024-02-22 14:13:11 +0000

    Added assets to the game. Space ship and space nebula background. Tweaked the code to fit the new scaled size of the object and added a cosnt of the initial position for later use.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,57 @@
 #include "main.h"
 
-int main()
-{
-    int windowWidth = 1500;
+int main() {
+    int windowWidth = 800;
     int windowHeight = 800;
-    char Title[]{"SpaceHopper"}; 
+    char Title[]{"SpaceHopper"};
 
-    //Space Ship Texture
-    //Texture2D ship = LoadTexture("Assets/ship.png");
-
-    Character shipData;
-    shipData.position.x = windowWidth / 2;
-    shipData.position.y = windowHeight - shipData.texture.height;
-    // shipData.rec.width= ship.width;
-    // shipData.rec.height = ship.height;
-
-
+    // Initialize the window
     InitWindow(windowWidth, windowHeight, Title);
 
-    while (!WindowShouldClose())
-    {
+    //Space Background
+    Texture2D spaceBackground = LoadTexture("Assets/backgroundImage.png");
+    //// spaceBackgrounds original size
+    Rectangle sourceSpaceBGRec = {0.0f, 0.0f, (float)spaceBackground.width, (float)spaceBackground.height};
+    //// spaceBackrgounds updated size
+    Rectangle destinationSpaceBGRec = {0.0f, 0.0f, (float)windowWidth, (float)windowHeight};
+
+    Vector2 backgroundOrigin = {0.0f, 0.0f};
+    
+    // Load the spaceship texture
+    
+    Character shipData;
+    shipData.texture = LoadTexture("Assets/ship.png"); 
+    float scale = 4.0f;
+    float scaledHeight = shipData.texture.height * scale;
+    
+
+    //Object Locations
+    shipData.position.x = windowWidth / 2 - scaledHeight / 2; // Center the ship
+    float floorPositionY = windowHeight - 50;
+    shipData.position.y = floorPositionY - scaledHeight + 55;  
+
+    // Use to check the ship has a safe landing
+    const float StarterPosition = shipData.position.y;
+
+    // Main game loop
+    while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        DrawRectangle(windowWidth/2, windowHeight - 100 - 100, 50, 100, BLUE);
+        DrawTexturePro(spaceBackground, sourceSpaceBGRec, destinationSpaceBGRec, backgroundOrigin, 0.0f, WHITE);
 
-        DrawRectangle(0, windowHeight - 100, windowWidth, 50, WHITE);
+        // Draw the spaceship texture
+        DrawTextureEx(shipData.texture, shipData.position, 0.0f, scale, WHITE);
+
+        // Draw the ground rectangle
+        DrawRectangle(0, floorPositionY, windowWidth, 10, WHITE);
 
         EndDrawing();
     }
 
-    CloseWindow();
+    // De-Initialization
+    UnloadTexture(shipData.texture); // Unload the texture
+    CloseWindow(); // Close the window
+
+    return 0;
 }
